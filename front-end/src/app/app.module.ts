0054f378532d1c9dd4498d3872e8bbe426635f4a
Paolo Tesser <p.tesser921@gmail.com>
2018-11-26 23:32:19 +0100

    feat (ui): add library as internal projects
    Use `ng generate library ui --prefix=ui` to generate it.
    
    Include also VMWare UI lib to use in our components.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// FIXME: Cannot use `@ui` for this: https://github.com/angular/vscode-ng-language-service/issues/79
+import { UiModule } from '../../projects/ui/src/lib/ui.module';
+
 import { AppRoutingModule } from '@rock-recipe/app-routing.module';
 import { AppComponent } from '@rock-recipe/app.component';
 
@@ -10,6 +13,7 @@ import { AppComponent } from '@rock-recipe/app.component';
   ],
   imports: [
     BrowserModule,
+    UiModule,
     AppRoutingModule,
   ],
   providers: [],
