fbd27fd73ea8181734add417045dfa56f62437c5
Luca <luca.vannucci@supermercato24.it>
2018-12-16 18:35:07 +0100

    feat(Controller) add methods to handle the services

--- back-end/app/Http/Controllers/IngredientsController.php
+++ back-end/app/Http/Controllers/IngredientsController.php
@@ -1,30 +1,68 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers;
+
+use App\Http\Requests\Ingredients;
+use App\Models\Ingredient;
+use Illuminate\Http\JsonResponse;
 
 class IngredientsController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Get all ingredients from DB.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        return response()->json(['code' =>"Tutti gli ingredienti"],200);
+        $data = Ingredient::getAllIngredient();
+        return response()->json(['data' => $data ],self::OK);
 
     }
 
     /**
-     * Display the specified resource.
+     * Get the specified ingredients from DB.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
+    public function show($id): JsonResponse
     {
-        return response()->json(['code'=>"Restituzione ingrediente"],200);
+        $data = Ingredient::getSpecifiedIngredients($id);
+        return response()->json(['data' => $data ],self::OK);
 
+    }
+
+    /**
+     * Method to create an Ingredient.
+     *
+     * @param Ingredients $request
+     *
+     * @return JsonResponse
+     */
+    public function create(Ingredients $request): JsonResponse
+    {
+        $name = $request->input('name');
+        $newIngredient = Ingredient::createIngredients($name);
+        return response()->json(['data' => $newIngredient],self::OK);
+
+    }
+
+    /**
+     * Method to delete an Ingredient.
+     *
+     * @param $id
+     *
+     * @return JsonResponse
+     */
+    public function delete($id)
+    {
+        $deleteIngredient = Ingredient::deleteIngredient($id);
+        if ($deleteIngredient === true) {
+            return response()->json(['data' => $deleteIngredient], self::OK);
+        } else {
+            return response()->json(['message' => 'Resource not found' ], self::NOT_FOUND);
+        }
     }
 
 }
