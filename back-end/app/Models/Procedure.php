0148986b1a2c950a5f0cf52ade2bd5b9c0b07737
Luca <luca.vannucci@supermercato24.it>
2019-01-01 23:26:46 +0100

    fix(procedure): add fillable field

--- back-end/app/Models/Procedure.php
+++ back-end/app/Models/Procedure.php
@@ -7,11 +7,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Procedure extends Model
 {
-    protected $table = ''
-    protected $fillable = ['name'];
+    /** @var string $table */
+    protected $table = 'procedures';
+
+    /** @var array $fillable */
+    protected $fillable = ['recipe_id','step'];
+
+    /** @var array $hidden */
+    protected $hidden = ['created_at', 'updated_at'];
 
     public function recipe(): BelongsTo
     {
-        return $this->belongsTo('App/Recipe');
+        return $this->belongsTo('App/Models/Recipe');
     }
 }
