e6f5287774221dffc359c547f04756af991b70f9
Luca <luca.vannucci@supermercato24.it>
2019-01-01 23:24:55 +0100

    feat(Recipe): modify handling for crud and repository pattern

--- back-end/app/Http/Controllers/RecipesController.php
+++ back-end/app/Http/Controllers/RecipesController.php
@@ -1,58 +1,106 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Recipes;
+use App\Repositories\Implementations\RecipeImpl;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RecipesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    /** @var RecipeImpl $recipeImp */
+    private $recipeImp;
+
+    public function __construct(RecipeImpl $recipeImp)
     {
-        return response()->json(['code' =>"Tutte le ricette"],200);
-
-    }
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        return response()->json(['code'=>"creazione Ricetta"],200);
-
+        $this->recipeImp = $recipeImp;
     }
 
     /**
-     * Display the specified resource.
+     * Get all Recipes from DB.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $data = $this->recipeImp->getAll();
+
+        return response()->json(['data' => $data], self::OK);
+    }
+
+    /**
+     * Get the specified Recipe from DB.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show($id): JsonResponse
     {
-        return response()->json(['code'=>"Restituzione Ricetta"],200);
+        $data = $this->recipeImp->getById($id);
 
+        return response()->json(['data' => $data], self::OK);
     }
 
     /**
-     * Update the specified resource in storage.
+     * Method to create a Recipe.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Recipes $request
+     *
+     * @return JsonResponse
      */
-    public function update(Request $request, $id)
+    public function create(Recipes $request): JsonResponse
     {
-        return response()->json(['code'=>"Update Ricetta"],200);
+        $params = $request->input();
 
+        $recipe =  $this->recipeImp->create($params);
+
+        if (!empty($recipe)) {
+            $res =  response()->json(['data' => $recipe], self::OK);
+        } else {
+            $res = response()->json(['data' => $recipe], self::NOT_CREATED);
+        }
+
+        return $res;
     }
 
+    /**
+     * Method to delete a Recipe.
+     *
+     * @param $id
+     *
+     * @return JsonResponse
+     */
+    public function delete($id)
+    {
+        $data = $this->recipeImp->delete($id);
+        if ($data === true) {
+            $response = response()->json(['data' => $data], self::OK);
+        } else {
+            $response = response()->json(['message' => 'Resource not found' ], self::NOT_FOUND);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Method to update Recipe data.
+     *
+     * @param $id
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function update($id, Request $request)
+    {
+        $params = $request->input();
+        $data = $this->recipeImp->update($id, $params);
+        if ($data === true) {
+            $response = response()->json(['data' => $data], self::OK);
+        } else {
+            $response = response()->json(['message' => 'Resource not found' ], self::NOT_FOUND);
+        }
+
+        return $response;
+    }
 }
--- back-end/app/Models/Recipe.php
+++ back-end/app/Models/Recipe.php
@@ -1,23 +1,38 @@
-<?php
+<?php declare(strict_types=1);
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Recipe extends Model
 {
-    protected $fillable = ['name','people'];
+    /** @var string $table */
+    protected $table = 'recipes';
+
+    /** @var array $fillable */
+    protected $fillable = ['name','people', 'user_id'];
+
+    /** @var string $belongs */
+    protected $belongs = 'recipes_ingredients';
+
+    /** @var array $hidden */
+    protected $hidden = ['created_at', 'updated_at'];
 
     public function ingredients(): BelongsToMany
     {
-        return $this->belongsToMany('App/Ingredient','recipes_ingredients','recipe_id','ingredients_id');
+        return $this->belongsToMany('App/Models/Ingredient',$this->belongs,'recipe_id','ingredients_id');
     }
 
     public function procedures(): HasMany
     {
-        return $this->hasMany('App/Procedure');
+        return $this->hasMany('App/Models/Procedure');
     }
 
+    public function users(): HasOne
+    {
+        return $this->hasOne('App/Models/User');
+    }
 }
