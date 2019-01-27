d801699fa66664f3bfdac86b6ef4edf52818cced
Luca <luca.vannucci@supermercato24.it>
2019-01-27 18:37:20 +0100

    feat(interface): add ingredient interface

---
+++ back-end/app/Repositories/Interfaces/Ingredient.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\Repositories\Interfaces;
+
+/**
+ * Interface Ingredient.
+ *
+ * interface to extend common methods.
+ */
+interface Ingredient extends Common
+{
+
+}
